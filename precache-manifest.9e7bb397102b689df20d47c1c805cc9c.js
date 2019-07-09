9ff70de815c448c4bc19b1ffe03f798016d4a91c
SachRestated <sachinvashisht1999@gmail.com>
2019-07-09 23:18:27 +0530

    Updates

---
+++ precache-manifest.9e7bb397102b689df20d47c1c805cc9c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c46879bfde93bc909bbab768a1c395d",
+    "url": "/HangmanGame/index.html"
+  },
+  {
+    "revision": "1099d6e26e254ff54ae7",
+    "url": "/HangmanGame/static/css/main.24ef05ef.chunk.css"
+  },
+  {
+    "revision": "ded73dc3830fb2d80c34",
+    "url": "/HangmanGame/static/js/2.b7dd99f8.chunk.js"
+  },
+  {
+    "revision": "1099d6e26e254ff54ae7",
+    "url": "/HangmanGame/static/js/main.a7bcfce2.chunk.js"
+  },
+  {
+    "revision": "7b92ad4d4d131a079439",
+    "url": "/HangmanGame/static/js/runtime~main.a3b87270.js"
+  },
+  {
+    "revision": "db9097e874e68b7ed7115ff05ffcb880",
+    "url": "/HangmanGame/static/media/0.db9097e8.jpg"
+  },
+  {
+    "revision": "fbff4784004cc97463c883477a0090d1",
+    "url": "/HangmanGame/static/media/1.fbff4784.jpg"
+  },
+  {
+    "revision": "c4ea9528b67823bd94c90de3d97d75a3",
+    "url": "/HangmanGame/static/media/2.c4ea9528.jpg"
+  },
+  {
+    "revision": "62a781c89c590abb27129638d9d18c4d",
+    "url": "/HangmanGame/static/media/3.62a781c8.jpg"
+  },
+  {
+    "revision": "0f8ca81ee9f7b2bc27102267f9e23864",
+    "url": "/HangmanGame/static/media/4.0f8ca81e.jpg"
+  },
+  {
+    "revision": "1b557aa7ccfa88b6b3f4723c27d3f2d6",
+    "url": "/HangmanGame/static/media/5.1b557aa7.jpg"
+  },
+  {
+    "revision": "c80394c2a96ffad09a9df4e4815745f2",
+    "url": "/HangmanGame/static/media/6.c80394c2.jpg"
+  }
+]);
